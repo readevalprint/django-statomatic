d36773850574ce6cc1e8e6447ddb7b1e5dabfd60
Tim Watts <tim@readevalprint.com>
2011-05-14 22:25:30 +0000

    updated template, but need to seperate my blog from blroject

--- statomatic.py
+++ statomatic.py
@@ -1,5 +1,7 @@
 '''
-Run this with $ python ./statomatic.py and go to http://localhost:8000/
+Run this with $ python ./statomatic.py runserver and go to http://localhost:8000/
+Deploy with $ python ./statomatic.py render and rsync your files to a 
+public_html folder.
 '''
 
 import os, sys, itertools
@@ -11,9 +13,9 @@ from BeautifulSoup import BeautifulSoup
 from markdown2 import markdown
 
 
-me = os.path.splitext(os.path.split(__file__)[1])[0]
 # helper function to locate this dir
 here = lambda x: os.path.join(os.path.abspath(os.path.dirname(__file__)), x)
+me = os.path.splitext(os.path.split(__file__)[1])[0]
 
 # SETTINGS
 DEBUG=TEMPLATE_DEBUG=True
@@ -25,6 +27,11 @@ DEPLOY_DIR = here('deploy')
 INSTALLED_APPS = ('django.contrib.markup',)
 
 def smart_render(template, context={}):
+    '''
+    takes a path and tries to render it directly or the index file within it.
+    TODO: can make pretty urls in the future by putting all files in folders and
+    naming them index
+    '''
     template = template.rstrip('/')
     for suffix in ['','/index.html','index.html']:
         try:
@@ -34,6 +41,10 @@ def smart_render(template, context={}):
     raise Http404
 
 def markdownify(rendered_template):
+    '''
+    parse a rendered temaplte for tags that have class="md" and replace the
+    contents of the tag withthe marked up results.
+    '''
     html = BeautifulSoup(rendered_template)
     for md in html.findAll('','md'):
         md.contents = BeautifulSoup(markdown(md.renderContents()))
@@ -45,7 +56,7 @@ def index(request, template):
     r.content  = markdownify(r.rendered_content)
     return r
 
-
+# TODO: process all blog templates to pull title from them and populate the context.
 def blog(request,template):
     template = 'blog/'+template.rstrip('/')
     return smart_render(template, context={'name':'bill'})
@@ -66,11 +77,9 @@ def render():
     from django.test.client import Client
     client = Client()
     for root, dirs, files in os.walk(CONTENT_DIR):
-        print "Current directory", root
-        print "Sub directories", dirs
-        print "Files", files 
         for f in files:
-             if f[0] != '.':
+            # ignore hidden files 
+            if f[0] != '.':
                 url = root.replace(CONTENT_DIR,'')+'/'+f
                 response = client.get(url)
                 out = os.path.join(DEPLOY_DIR,url[1:])
@@ -80,14 +89,21 @@ def render():
                 f = open(out, 'w')
                 f.write(response.content)
                 f.close()
+
 def run():
     sys.path += (here('.'),)
     # set the ENV
     os.environ['DJANGO_SETTINGS_MODULE'] = me
-    #run the development server
-    from django.core import management
-    # management.call_command('runserver','0.0.0.0:8000' )
-    management.execute_from_command_line()
-
+ 
 if __name__ == '__main__':
+    from django.core import management
     run()
+    try:
+        if sys.argv[1] == 'render':
+                render()
+        elif sys.argv[1] == 'runserver':
+                management.call_command('runserver','0.0.0.0:8000' )
+        else:
+            print "please use with 'render' or 'runserver'"
+    except:
+        print "please use with 'render' or 'runserver'"
